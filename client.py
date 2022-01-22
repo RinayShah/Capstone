6938fafc63937f15fc69db43cf72921025e048d7
Samdisha Basra <samdisha.basra@ryerson.ca>
2022-01-21 21:18:42 -0500

    handles connection and registration

--- client.py
+++ client.py
@@ -1,17 +1,30 @@
 import socket
 
 # Create Socket
-sock = socket.socket()
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Define port number
 port = 8000
 
 # connect to the server on local computer
-sock.connect(('127.0.0.1', port))
+socket.connect(('127.0.0.1', port))
 
-# receive data from server
-print(sock.recv(1024).decode())
+# User input for potential Vehicle Name (Registration)
+response = socket.recv(2048)
+name = input(response.decode())
+socket.send(str.encode(name))
+
+# User input for Key -> To be modified so that it asks for key
+# only if already registered, otherwise system assigns key
+response = socket.recv(2048)
+key = input(response.decode())
+socket.send(str.encode(key))
+
+# Receive status from Server
+response = (socket.recv(2048))
+response = response.decode()
+print(response)
 
 # Close Connection
-sock.close()
+socket.close()
 
--- server.py
+++ server.py
@@ -1,28 +1,86 @@
 import socket
+import threading
+import hashlib
 
 # Create Socket
-sock = socket.socket()
-
-print ("Socket successfully created")
+socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+print("Socket successfully created")
 
 # reserve port number
 port = 8000
 
-# Bind socket to port and set to listening
-sock.bind(('', port))
-print ("socket binded to %s" %(port))
-sock.listen(5)
-print ("socket is listening")
+# Handling multiple connections (possibly)
+numConnections = 0
+
+# Bind socket to port or print error
+try:
+    socket.bind(('', port))
+except socket.error as e:
+    print(str(e))
+
+# Socket listening
+print("socket binded to %s" % port)
+print("socket is waiting for connection")
+socket.listen(5)
+
+# Hash Table for Clients (i.e., vehicles)
+HashTable = {}
+
+
+def client_registration(connection):
+
+    connection.send(str.encode('Registration Name: '))
+    name = connection.recv(2048)
+    name = name.decode()
+
+    # Key for registration (password) -> to be updated based on security protocol
+    connection.send(str.encode('Registration Key: '))
+    key = connection.recv(2048)
+    key = key.decode()
+
+    # key hash
+    key = hashlib.sha256(str.encode(key)).hexdigest()
+
+    # If not in HashTable, register
+    if name not in HashTable:
+        HashTable[name] = key
+        connection.send(str.encode('Vehicle has been Registered.'))
+        print(name, ' has been Registered.')
+        print("{:<8} {:<20}".format('Name', 'Key'))
+
+        for k, v in HashTable.items():
+            label, num = k, v
+            print("{:<8} {:<20}".format(label, num))
+        print("______________________________________________")
+
+    # Check password if already in HashTable (we may not need this part)
+    else:
+        if HashTable[name] == key:
+            connection.send(str.encode('Successful (key matches).'))
+            connection.send(name, ' Successful in Connecting.')
+        else:
+            connection.send(str.encode('Unsuccessful (key does not match'))
+            print(name, ' Unsuccessful in Connecting.')
+
+    while True:
+        break
+    connection.close()
+
 
 # While loop (main loop)
 while True:
 
     # Connect to Client
-    c, addr = sock.accept()
-    print ('Got connection from', addr )
+    Client, address = socket.accept()
 
-    c.send('Thank you for connecting'.encode())
+    clientHandler = threading.Thread(
+        target=client_registration,
+        args=(Client,)
+    )
 
-    # Close Client Connection and break from while loop
-    c.close()
-    break
+    clientHandler.start()
+    numConnections += 1
+
+    print('Connection Request Number: ' + str(numConnections))
+
+socket.close()
